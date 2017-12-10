b9818c62ac6c299f754a49975a9e0ec1dddf3a9e
WeiMingLin <linweimingcn@gmail.com>
2017-12-10 11:18:50 +0800

    初步设计主函数

---
+++ main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "mymath.h"
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+char inputbuffer[10];
+char calculatorbuffer[1024];
+
+int main(int argc, char *argv[]) {
+	int counter = 0;
+	char get = getchar();
+	while(get != EOF) {
+		if(get != '\n') {
+			inputbuffer[counter] = get;
+			counter++;
+			if(counter == 10) {
+				printf("\n\tThe input expression is too long.\n");
+			}
+		} else {
+			for(int i = 0; i < counter; i++) {
+				calculatorbuffer[i] = inputbuffer[i];
+				inputbuffer[i] = 0;
+			}
+			counter = 0;
+			getexpression(calculatorbuffer);
+		}
+		get = getchar();
+	}
+	
+	return 0;
+}
---
+++ mymath.h
@@ -0,0 +1,11 @@
+#ifndef __mymath_h__
+#define __mymath_h__
+
+#include <math.h>
+
+char* operation(char* expression)
+{
+	
+}
+
+#endif
