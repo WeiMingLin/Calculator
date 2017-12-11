8e63e1f5099a98d456f23272aeeb54b17f323d26
WeiMingLin <linweimingcn@gmail.com>
2017-12-11 10:47:51 +0800

    修改结束程序方式

--- mystring.c
+++ mystring.c
@@ -1,8 +1,12 @@
 void getexpressoion(char[] inputbuffer,char[] claculatorbuffer)
 {
 	gets(inputbuffer);
-	for(int i = 0; inputbuffer[i] != '\n' && inputbuffer[i] != EOF; i++) {
+	for(int i = 0; inputbuffer[i] != '\n'; i++) {
 		calculatorbuffer[i] = inputbuffer[i];
 		inputbuffer[i] = 0;
+		if(inputbuffer[i] == EOF) {
+			inputbuffer[0] = EOF;
+			break;
+		}
 	}
 }
